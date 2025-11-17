157d49b84611e41e0402219ed6521399982ef07f
Yaschilal <yasiada14@gmail.com>
2025-11-17 22:22:36 +0300

    tg remov 2nd please

--- server.js
+++ server.js
@@ -52,4 +52,6 @@ app.post('/api/contact', (req, res) => {
 });
 
 const PORT = process.env.PORT || 48253;
-app.listen(PORT, () => console.log(`🚀 Server running on this  port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
+});
